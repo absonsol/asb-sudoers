1463ffa6ab7fec524abc1e1f6ce120df64453ea2
Jordi Prats <jprats@systemadmin.es>
2016-06-10 13:26:11 +0200

    testing

---
+++ spec/acceptance/base_spec.rb
@@ -0,0 +1,30 @@
+require 'spec_helper_acceptance'
+require_relative './version.rb'
+
+describe 'sudoers class' do
+
+  context 'basic setup' do
+    # Using puppet_apply as a helper
+    it 'should work with no errors' do
+      pp = <<-EOF
+
+      class { 'sudoers': }
+
+      sudoers::sudo { 'rekcuf':
+        withoutpassword => true,
+      }
+
+      EOF
+
+      # Run it twice and test for idempotency
+      expect(apply_manifest(pp).exit_code).to_not eq(1)
+      expect(apply_manifest(pp).exit_code).to eq(0)
+    end
+
+    describe package($packagename) do
+      it { is_expected.to be_installed }
+    end
+
+  end
+
+end
