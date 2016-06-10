c876040b940394760185f50bb90e08e096084ca6
Jordi Prats <jprats@systemadmin.es>
2016-06-10 13:58:49 +0200

    testin valors per defectre

--- spec/acceptance/base_spec.rb
+++ spec/acceptance/base_spec.rb
@@ -14,6 +14,10 @@ describe 'sudoers class' do
         withoutpassword => true,
       }
 
+      sudoers::sudo { 'ualoc':
+        withoutpassword => true,
+      }
+
       EOF
 
       # Run it twice and test for idempotency
@@ -23,6 +27,23 @@ describe 'sudoers class' do
 
     describe package($packagename) do
       it { is_expected.to be_installed }
+    end
+
+    describe file('/etc/sudoers') do
+      it { should be_file }
+      its(:content) { should match 'puppet managed file' }
+      its(:content) { should match 'Defaults  !requiretty' }
+      its(:content) { should match 'Defaults  !visiblepw' }
+    end
+
+    describe file('/etc/sudoers.d/10_10_sudo_rekcuf') do
+      it { should be_file }
+      its(:content) { should match 'rekcuf ALL=(ALL) NOPASSWD: ALL' }
+    end
+
+    describe file('/etc/sudoers.d/10_10_sudo_ualoc') do
+      it { should be_file }
+      its(:content) { should match 'ualoc ALL=(ALL) ALL' }
     end
 
   end
