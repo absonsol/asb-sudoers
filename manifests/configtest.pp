a588a0004c2773bbf1d3591efd1934a010bb21c1
Aamir Shahzad Bhatti <asbkwt@gmail.com>
2022-05-28 21:54:03 +0200

    Update configtest.pp

--- manifests/configtest.pp
+++ manifests/configtest.pp
@@ -1,5 +1,5 @@
 class sudoers::configtest inherits sudoers::params {
-  exec { 'eyp-sudoers visudo check':
+  exec { 'asb-sudoers visudo check':
     command => 'visudo -c',
     unless  => 'visudo -c',
     path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
